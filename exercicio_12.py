ede0607283463e8c92e64ab49c1ad9cf84eef538
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:54:55 -0300

    Add: Adicionando décimo segundo exercicio

---
+++ exercicio_12.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+nome = str(input('Digite seu nome completo: '))
+nome_minuscula = nome.lower()
+
+print(f"{nome_minuscula}")
