0375465bb3db81d60fbdc26f69b430dedc9ce220
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:42:58 -0300

    Add: Adicionando o oitavo exercicio

---
+++ exercicio_08.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+base = int(input('Digite a base: '))
+expoente = float(input('Digite o expoente: '))
+
+potencia = base ** expoente
+
+print(f"{base} na potência de {expoente} é igual a {potencia}")
