1ef695b8ec198c619c38dedbf23b63da7702da05
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:53:15 -0300

    Add: Adicionando o décimo primeiro exercício

---
+++ exercicio_11.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+texto = str(input('Digite algum texto: '))
+texto_maiuscula = texto.upper()
+
+print(f"{texto_maiuscula}")
