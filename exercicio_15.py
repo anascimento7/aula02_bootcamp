8691a3c84c9354cf00461aeee096b3fab1013015
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 11:04:35 -0300

    Add: Adicionando exercicio 15

---
+++ exercicio_15.py
@@ -0,0 +1,8 @@
+# Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+texto_um = str(input('Digite um texto: '))
+texto_dois = str(input('Digite outro texto: '))
+
+texto_concatenado = texto_um + texto_dois
+
+print(f"{texto_concatenado}")
