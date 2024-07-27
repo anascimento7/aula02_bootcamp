b86d3d69110d3838783b6b361a927a29256fdfe1
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:25:19 -0300

    Add: Adicionado segundo exercicio

---
+++ exercicio_02.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero = int(input('Digite um número inteiro: '))
+
+divisao = numero % 5
+
+print(divisao)
