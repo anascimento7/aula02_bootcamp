5a540fcac6213d77a3ff461a4199a98adfe6e12d
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:29:54 -0300

    Add: Adicionando quarto exercicio

---
+++ exercicio_04.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero_um = int(input('Digite o primeiro número: '))
+numero_dois = int(input('Digite o segundo número: '))
+
+divisao_inteira = numero_um // numero_dois
+
+print(f"A divisão inteira de {numero_um} por {numero_dois} é de {divisao_inteira}")
