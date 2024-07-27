ab2b8cbdb5a2bfad0eec471b64783d301a47104a
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:35:18 -0300

    Add: Adicionando sexto exercicio

---
+++ exercicio_06.py
@@ -0,0 +1,8 @@
+# Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+num_01 = float(input('Digite um número: '))
+num_02 = float(input('Digite outro número: '))
+
+soma = num_01 + num_02
+
+print(f"A soma de {num_01} com {num_02} é igual a {soma}")
