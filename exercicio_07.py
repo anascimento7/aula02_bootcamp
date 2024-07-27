91bdad784007cdc3cfab8413c1b88b6271807bbd
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 10:37:56 -0300

    Add: Adicionando sétimo exercicio

---
+++ exercicio_07.py
@@ -0,0 +1,8 @@
+# Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+num_01 = float(input('Digite o primeiro número: '))
+num_02 = float(input('Digite o segundo número: '))
+
+media = (num_01 + num_02) / 2
+
+print(f"A média dos números fornecidos é de {media}")
